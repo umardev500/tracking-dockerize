79e500e4f46366d127dc660fa13c4d809cfc083c
umardevid <umaralfaruq587@gmail.com>
2021-06-23 13:41:47 +0700

    auto migrate

--- go/main.go
+++ go/main.go
@@ -5,11 +5,14 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/umardevid/go-resi-ongkir/migration"
 	"github.com/umardevid/go-resi-ongkir/router"
 )
 
 func main() {
 	port := os.Getenv("PORT")
+	migration.Migrasi()
+
 	e := echo.New()
 
 	e.Use(middleware.CORS())
--- go/migration/migrations.go
+++ go/migration/migrations.go
@@ -1,62 +1,62 @@
-package main
+package migration
 
 import (
-	"fmt"
+    "fmt"
 
-	"github.com/umardevid/go-resi-ongkir/entity"
-	"github.com/umardevid/go-resi-ongkir/models"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+    "github.com/umardevid/go-resi-ongkir/entity"
+    "github.com/umardevid/go-resi-ongkir/models"
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
 )
 
-func main() {
-	dsn := "root:hello@tcp(localhost)/waybill_cost?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+func Migrasi() {
+    dsn := "root:hello@tcp(localhost)/waybill_cost?charset=utf8mb4&parseTime=True&loc=Local"
+    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
 
-	// Drop table
-	// to disable auto drop then you can escape single line of code below
-	// START
-	db.Migrator().DropTable(&models.CourierModel{}, &models.ContactModel{}, &entity.Admin{})
-	// END
+    // Drop table
+    // to disable auto drop then you can escape single line of code below
+    // START
+    db.Migrator().DropTable(&models.CourierModel{}, &models.ContactModel{}, &entity.Admin{})
+    // END
 
-	// Running auto migrate
-	db.AutoMigrate(&models.CourierModel{}, &models.ContactModel{}, &entity.Admin{})
+    // Running auto migrate
+    db.AutoMigrate(&models.CourierModel{}, &models.ContactModel{}, &entity.Admin{})
 
-	// Insert dummy data
-	// Escape insert line if you not want from start to end
-	// START
-	courier := models.CourierModel{Title: "Cek resi wahana", Slug: "cek-resi-wahana", Value: "wahana", Description: "Description wahana"}
-	db.Create(&courier)
-	courier2 := models.CourierModel{Title: "Cek resi jnt", Slug: "cek-resi-jnt", Value: "jnt", Description: "Description for jnt"}
-	db.Create(&courier2)
-	courier3 := models.CourierModel{Title: "Cek resi pos", Slug: "cek-resi-pos", Value: "pos", Description: "Description pos"}
-	db.Create(&courier3)
+    // Insert dummy data
+    // Escape insert line if you not want from start to end
+    // START
+    courier := models.CourierModel{Title: "Cek resi wahana", Slug: "cek-resi-wahana", Value: "wahana", Description: "Description wahana"}
+    db.Create(&courier)
+    courier2 := models.CourierModel{Title: "Cek resi jnt", Slug: "cek-resi-jnt", Value: "jnt", Description: "Description for jnt"}
+    db.Create(&courier2)
+    courier3 := models.CourierModel{Title: "Cek resi pos", Slug: "cek-resi-pos", Value: "pos", Description: "Description pos"}
+    db.Create(&courier3)
 
-	contact := models.ContactModel{Email: "first@gmail.com", Message: "First message"}
-	db.Create(&contact)
-	contact1 := models.ContactModel{Email: "second@gmail.com", Message: "Second message"}
-	db.Create(&contact1)
-	contact2 := models.ContactModel{Email: "third@gmail.com", Message: "Third message"}
-	db.Create(&contact2)
-	// END
+    contact := models.ContactModel{Email: "first@gmail.com", Message: "First message"}
+    db.Create(&contact)
+    contact1 := models.ContactModel{Email: "second@gmail.com", Message: "Second message"}
+    db.Create(&contact1)
+    contact2 := models.ContactModel{Email: "third@gmail.com", Message: "Third message"}
+    db.Create(&contact2)
+    // END
 
-	// Set admin password
-	user := entity.Admin{User: "admin", Pass: "admin"}
-	db.Create(&user)
+    // Set admin password
+    user := entity.Admin{User: "admin", Pass: "admin"}
+    db.Create(&user)
 
-	// To update admin password unescape this line from start to end
-	// START
-	/*
-		var admin entity.Admin
-		db.Where("user = ? and pass = ?", "admin", "admin").First(&admin)
-		admin.User = "hello"
-		admin.Pass = "world"
-		db.Save(&admin)
-	*/
-	// END
+    // To update admin password unescape this line from start to end
+    // START
+    /*
+        var admin entity.Admin
+        db.Where("user = ? and pass = ?", "admin", "admin").First(&admin)
+        admin.User = "hello"
+        admin.Pass = "world"
+        db.Save(&admin)
+    */
+    // END
 
 }
