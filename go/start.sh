fa93e2282e2ac285747f78866e93d3c10ec053dd
umardevid <umaralfaruq587@gmail.com>
2021-06-23 11:32:18 +0700

    setup go

--- go/start.sh
+++ go/start.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-export PORT=":5000" # you can change this port
+export PORT=":8000" # you can change this port
 export RAJAONGKIRKEY="553c7982c1e8205443b4ad8d584e399e"
 # root = user hello = password waybill_cost=database
 export DSN="server:hello@tcp(db:3306)/waybill_cost?charset=utf8mb4&parseTime=True&loc=Local"
