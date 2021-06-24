47a3d2ea0fb2f6d378b274fcdc0b3e6f1bb5681b
umardevid <umaralfaruq587@gmail.com>
2021-06-24 10:56:15 +0700

    adding section and change href

--- client/pages/cek-ongkir.tsx
+++ client/pages/cek-ongkir.tsx
@@ -42,6 +42,11 @@ const CostCheck = (): JSX.Element => {
           <Form />
           <Result />
         </div>
+        <div className="col-span-2 mt-28 lh-lg mb-10">
+          <h2 className="text-2xl font-roboto font-medium mb-4">Section name</h2>
+          <p className="text-gray-600 font-roboto mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio molestias delectus neque repudiandae dolorem dolores. Officiis facilis rem ipsam qui excepturi commodi non, ab, ducimus quia alias nam nihil illum.</p>
+          <p className="text-gray-600 mt-4 font-roboto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio molestias delectus neque repudiandae dolorem dolores. Officiis facilis rem ipsam qui excepturi commodi non, ab, ducimus quia alias nam nihil illum.</p>
+        </div>
       </div>
       <Footer />
     </>
--- client/pages/cek-resi.tsx
+++ client/pages/cek-resi.tsx
@@ -19,6 +19,11 @@ export default function WaybillCheck(): JSX.Element {
       <div className="px-4 lg:px-32 pb-20">
         <Hero title="Cek Resi" description="Lacak status dan posisi pesanana anda dari berbagai kurir di Indonesia" />
         <Waybill />
+        <div className="col-span-2 mt-28 lh-lg mb-10">
+          <h2 className="text-2xl font-roboto font-medium mb-4">Section name</h2>
+          <p className="text-gray-600 font-roboto mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio molestias delectus neque repudiandae dolorem dolores. Officiis facilis rem ipsam qui excepturi commodi non, ab, ducimus quia alias nam nihil illum.</p>
+          <p className="text-gray-600 mt-4 font-roboto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio molestias delectus neque repudiandae dolorem dolores. Officiis facilis rem ipsam qui excepturi commodi non, ab, ducimus quia alias nam nihil illum.</p>
+        </div>
       </div>
       <Footer />
     </>
