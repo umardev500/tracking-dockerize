025ffc3866c24c490b77d9ea538fdac288f4cc13
umardevid <umaralfaruq587@gmail.com>
2021-09-11 21:58:00 +0700

    use dummy gtag

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -25,6 +25,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     <Provider store={store}>
       <Script
         strategy="afterInteractive"
+        async
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Script
